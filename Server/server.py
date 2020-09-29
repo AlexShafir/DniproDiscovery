df3d127be259ae54570e5b90bbe734e147634b19
Oleksandr Shafir <AlexShafir@users.noreply.github.com>
2020-09-29 17:58:17 +0300

    Fix tabs

--- Server/server.py
+++ Server/server.py
@@ -5,7 +5,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-	self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(b'Hello, world!')
 
